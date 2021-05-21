630e988afadbf1594cf66afde2a9338660427042
jronca21 <jpronca03@gmail.com>
2021-05-21 14:40:06 +0000

    added select random movie method

--- movie-selector.py
+++ movie-selector.py
@@ -1,6 +1,14 @@
 import csv
+import random
+
+def select_movie(f):
+    reader = csv.reader(f)
+    chosen_row = random.choice(list(reader))
+    print(chosen_row)
 
 with open('movie-list.csv') as f:
     movies = csv.DictReader(f)
-    for row in movies:
-        print(row['title'])
+    # for row in movies:
+    #     print(row['title'])
+    select_movie(f)
+
